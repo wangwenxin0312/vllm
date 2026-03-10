394785634fbc83bc72a04e3e7bf6f45524a47800
wenxinwang <wangwenxin21@huawei.com>
2026-03-10 06:10:44 -0700

    Fix CUDA full graph for DeepSeek implementation

--- vllm/v1/attention/backends/mla/flashmla.py
+++ vllm/v1/attention/backends/mla/flashmla.py
@@ -137,7 +137,10 @@ class FlashMLAMetadataBuilder(MLACommonMetadataBuilder[FlashMLAMetadata]):
             #  it needs to monotonically increasing by 1)
             self.cg_buf_num_splits[n:].fill_(num_splits[-1])
             num_splits = num_splits_view
-            topk_tile_scheduler_metadata, topk_num_splits, topk_seq_lens  = ucm_sparse.maybe_init_cudagraph_buffers_for_topk(n, tile_scheduler_metadata, topk_tile_scheduler_metadata, topk_num_splits, topk_seq_lens)
+            if has_ucm_sparse():
+                ucm_sparse = get_ucm_sparse()
+                if os.getenv("VLLM_HASH_ATTENTION") == "1":
+                    topk_tile_scheduler_metadata, topk_num_splits, topk_seq_lens  = ucm_sparse.maybe_init_cudagraph_buffers_for_topk(n, tile_scheduler_metadata, topk_tile_scheduler_metadata, topk_num_splits, topk_seq_lens)
 
         return FlashMLADecodeMetadata(
             block_table=block_table_tensor,
